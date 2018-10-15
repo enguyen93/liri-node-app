7e1138dcf3084298eec1fc84e9959db41b5a5d69
enguyen93 <enguyenusaf93@gmail.com>
2018-10-15 15:44:39 -0500

    waiting

--- Javascript/liri.js
+++ Javascript/liri.js
@@ -1,7 +1,32 @@
-var thing = require("dotenv").config();
+//imports
+require("dotenv").config({ path: "../.env" });
 var keysImport = require("./keys.js");
+var Spotify = require("node-spotify-api");
 
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keysImport.spotify);
 
-console.log("something");
-console.log(keysImport);
+var inputString = process.argv;
+
+var command = inputString[2];
+var artistBand = inputString[3];
+
+//Spotify for songs
+//Bands in Town for concerts
+//OMDB for movies
+
+if (command === "spotify-this-song") {
+    spotify.search({type: 'track', query: artistBand }, function (err, data) {
+        if (err) {
+            return console.log("error" + err);
+        }
+        console.log(data);
+    });
+}
+
+if (command === "concert-this") {
+
+}
+
+if (command === "movie-this") {
+    
+}
