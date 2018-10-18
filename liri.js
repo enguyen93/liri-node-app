e7817c440b93c584afc464478870cbaeab40e8ec
enguyen93 <enguyenusaf93@gmail.com>
2018-10-18 15:16:41 -0500

    last function needs working var

--- liri.js
+++ liri.js
@@ -2,84 +2,47 @@
 require("dotenv").config({ path: ".env" });
 var keysImport = require("./keys.js");
 var Spotify = require("node-spotify-api");
-
 var spotify = new Spotify(keysImport.spotify);
 
+var request = require("request");
+var fs = require("fs");
+
+
 var inputString = process.argv;
-
 var command = inputString[2];
-var artistBand = inputString[3];
+var thingToLookUp = inputString[3];
 
 //Spotify for songs
 //Bands in Town for concerts
 //OMDB for movies
 
-// if (command === "spotify-this-song") {
-//     spotify.search({type: 'track', query: artistBand + "&limit=1" }, function (err, data) {
-//         if (err) {
-//             return console.log("error" + err);
-//         }
-//         // console.log(data.tracks.items[0].album.name);
-//         var shortHand = (data.tracks.items[0].album.artists[0]);
-//         var albumShorthand = (data.tracks.items[0].album.name);
 
-//         console.log("The song name is: " + artistBand);
-//         console.log("The artist of this track is: " + shortHand.name);
-//         console.log("The Spotify Preview URL: " + shortHand.href);
-//         console.log("The Album name is: " + albumShorthand);
-
-
-//     });
-// }
-
-
-
-//Spotify for songs
-//Bands in Town for concerts
-//OMDB for movies
-
-// switch (command) {
-//     case "spotify-this-song":
-//         var songName = argument;
-//         if (songName === "") {
-//             spotify.lookup({ type: 'track', id: '3DYVWvPh3kGwPasp7yjahc' }, function (err, data) {
-//                 if (err) {
-//                     console.log(err);
-//                     return;
-//                 }
-//                 logOutput("Artist: " + data.artists[0].name);
-//                 logOutput("Song: " + data.name);
-//                 logOutput("Spotify Preview URL: " + data.preview_url);
-//                 logOutput("Album Name: " + data.album.name);
-//             })
-//         }
-//         spotifyThisSong();
-// }
-// console.log(spotify.lookup(type: 'track', 
-
-
+//working
 switch (command) {
     case "spotify-this-song":
-        if (artistBand === "") {
+        if (thingToLookUp === "") {
             predefinedSong();
         }
         spotifyThisSong();
         break;
-
-    // case "concert-this":
+    case "concert-this":
+        concertThis();
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
 }
 
 
 function spotifyThisSong() {
-
-    spotify.search({ type: 'track', query: artistBand + "&limit=1" }, function (err, data) {
+    //working
+    spotify.search({ type: 'track', query: thingToLookUp + "&limit=1" }, function (err, data) {
         var shortHand = (data.tracks.items[0].album.artists[0]);
         var albumShorthand = (data.tracks.items[0].album.name);
         if (err) {
             return console.log("error" + err);
         }
         else {
-            console.log("The song name is: " + artistBand);
+            console.log("The song name is: " + thingToLookUp);
             console.log("The artist of this track is: " + shortHand.name);
             console.log("The Spotify Preview URL: " + shortHand.href);
             console.log("The Album name is: " + albumShorthand);
@@ -96,10 +59,41 @@ function predefinedSong() {
         if (err) {
             return console.log("error" + err);
         }
-        console.log("The song name is: " + artistBand);
+        console.log("The song name is: " + thingToLookUp);
         console.log("The artist of this track is: " + shortHand.name);
         console.log("The Spotify Preview URL: " + shortHand.href);
         console.log("The Album name is: " + albumShorthand);
     })
-
+    //for some reason its returning a different band search
 }
+
+function concertThis(thingToLookUp) {
+    var bandsInTownURL = "https://rest.bandsintown.com/artists/" + thingToLookUp + "/events?app_id=codingbootcamp";
+
+    request(bandsInTownURL, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            console.log(body);
+        }
+    })
+}
+//only returns an empty array
+//node liri.js concert-this "all time low"
+
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log(data);
+
+        var dataArr = data.split(",");
+        // console.log(dataArr);
+        var newCommand = dataArr[0];
+        var newSong = dataArr[1];
+        //figure out how to use the newSong variable to replace "thingToLookUp" that's in the function
+        spotifyThisSong(newSong)
+    }
+    )
+};  
+//not working properly returns some other song, william ryan fritch
